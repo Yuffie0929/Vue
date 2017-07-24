02bbfaabe61f5134a6f104a6d917dd1cf0eb6be6
Yuffie0929 <441843578@qq.com>
2017-07-24 15:59:45 +0800

    update

--- task01/app.js
+++ task01/app.js
@@ -1,12 +1,20 @@
 /**
  * Created by Yuffie on 2017/7/17.
  */
+var list = JSON.parse(localStorage.getItem('todoList')) || [
+        { title: "吃饭", isChecked: true },
+        { title: "睡觉", isChecked: false },
+    ];
 let data = {
+    active: 'ALL', //ALL TODO HAVEDOWN
     todo: '',
     allList: 0,
     todoList: 0,
     haveDownList: 0,
-    list: []
+    list: list,
+    showList: list,
+    editingItem: {},
+    beforeEditTitle: "",
 };
 /**
  * this在事件中指向的是根实例
@@ -16,12 +24,81 @@ var vm = new Vue({
     data: data,
     methods: {
         addTodo(num, ev){
-            console.log(num, ev, this);
-            this.list.push({ title: this.todo });
+            if(this.todo.length == 0) return;
+            this.list.push({ title: this.todo, isChecked: false});
             this.todo = '';
         },
-        delTodo(ev){
-
+        delTodo(index){
+            this.list.splice(index, 1);
+        },
+        changeChecked: function (ev, item){
+            console.log(ev);
+            item.isChecked = ev.target.checked;
+        },
+        changeList: function (type){
+            this.active = type;
+            return this.list
+        },
+        saveStorage: function (){
+            localStorage.setItem('todoList', JSON.stringify(this.list));
+        },
+        editListItem: function (item, el){
+            this.editingItem = item;
+            this.beforeEditTitle = item.title;
+        },
+        saveEdit: function (){
+            this.editingItem = {};
+        },
+        cancelEdit: function (){
+            this.editingItem.title = this.beforeEditTitle;
+            this.editingItem = {};
+        },
+    },
+    directives: {
+        focus: {
+            update(el, binding){
+                if(binding.value){
+                    el.focus();
+                }
+            }
         }
+    },
+    computed: {
+        allLength: function (){
+            return this.list.length;
+        },
+        checkedLength: function (){
+            return this.list.filter(function (item){
+                return item.isChecked;
+            }).length
+        },
+        nocheckedLength: function (){
+            return this.list.filter(function (item){
+                return !item.isChecked;
+            }).length
+        },
+    },
+    watch: {
+        list: {
+            handler: function (){
+                this.saveStorage();
+            },
+            deep: true,
+        },
+        active(curVal,oldVal){
+            if(curVal == "ALL"){
+                this.showList = this.list;
+            }
+            if(curVal == "TODO"){
+                this.showList = this.list.filter(function (item){
+                    return !item.isChecked;
+                });
+            }
+            if(curVal == "HAVEDOWN"){
+                this.showList = this.list.filter(function (item){
+                    return item.isChecked;
+                });
+            }
+        },
     }
 });
